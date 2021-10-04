fc3f7673719669b35c033c32cff7b2c07277c8cc
jonatan <20198438@itla.edu.do>
2021-10-03 20:00:03 -0400

    Inicio del proyecto bosa PIII

---
+++ Bolsa de Empleos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bolsa_de_Empleos.MvcApplication" Language="C#" %>
---
+++ Bolsa de Empleos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bolsa_de_Empleos.Startup))]
+namespace Bolsa_de_Empleos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
